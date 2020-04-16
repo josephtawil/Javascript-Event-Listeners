50917d75f96439f7f15822d808435d1474ff22ad
Joseph Tawil <jtawil2015@gmail.com>
2020-04-15 18:43:49 -0700

    created a variable and set attributes with javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+//create a variable called h1Target and it must target the proper id from html page
+
+var h1Target = document.querySelector("#target");
+
+//give a new style attribute to the h1Target and set the color to red
+
+h1Target.setAttribute("style", "color: red");
